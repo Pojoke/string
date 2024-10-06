f6cb0f8da3e0f4ee40733f64bdcd922dfe87809f
Pojoke <pavlykdmutro@gmail.com>
2024-10-06 07:14:00 +0300

    dz-06

---
+++ dz/function.php
@@ -0,0 +1,35 @@
+<?php
+
+function check($text)
+{
+  
+    $odna_cov = substr_count($text, "'");
+    $dve_cov = substr_count($text, '"');
+    $duzky = substr_count($text, '()');
+if($duzky%2!==0){
+    echo "Кількість дужок не співпадає!\n";
+}
+if(  $duzky%2==0){
+    echo "Кількість дужок  співпадає!\n";
+}
+
+    
+    if ($odna_cov % 2 !== 0) {
+        echo "Кількість одинарних лапок не співпадає!\n";
+    }
+
+    if ($dve_cov % 2 !== 0) {
+        echo "Кількість парних лапок не співпадає!!\n";
+    }
+else{
+    $odna_cov % 2 === 0 && $dve_cov % 2 === 0;
+    echo "Всі лапки закриті\n";
+}
+    // if ( $odna_cov % 2 === 0 && $dve_cov % 2 === 0) {
+    //     echo "Все кавычки парные.\n";
+    // }
+}
+
+// Пример использования
+$text = '"Hello""; "print((($a + ($b))))";';
+check($text);
