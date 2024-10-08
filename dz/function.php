f920c7218545a13516e1bb4d9d34f38ca3c5c5c9
Pojoke <pavlykdmutro@gmail.com>
2024-10-08 07:05:23 +0300

    dorobiv

--- dz/function.php
+++ dz/function.php
@@ -6,23 +6,23 @@ function check($text)
     $odna_cov = substr_count($text, "'");
     $dve_cov = substr_count($text, '"');
     $duzky = substr_count($text, '()');
-if($duzky%2!==0){
-    echo "Кількість дужок не співпадає!\n";
-}
-if(  $duzky%2==0){
-    echo "Кількість дужок  співпадає!\n";
-}
+    if($duzky%2!==0){
+        echo "Кількість дужок не співпадає!\n";
+    }
+    else{
+        echo "Кількість дужок  співпадає!\n";
+    }
 
     
     if ($odna_cov % 2 !== 0) {
         echo "Кількість одинарних лапок не співпадає!\n";
     }
 
-    if ($dve_cov % 2 !== 0) {
+    elseif ($dve_cov % 2 !== 0) {
         echo "Кількість парних лапок не співпадає!!\n";
     }
 else{
-    $odna_cov % 2 === 0 && $dve_cov % 2 === 0;
+  //  $odna_cov % 2 === 0 && $dve_cov % 2 === 0;
     echo "Всі лапки закриті\n";
 }
     // if ( $odna_cov % 2 === 0 && $dve_cov % 2 === 0) {
