b16db663816743bac5a99e936a1d52c729856b1a
Pojoke <pavlykdmutro@gmail.com>
2024-10-16 08:22:34 +0300

    got

--- dz/photo.php
+++ dz/photo.php
@@ -1,8 +1,7 @@
 <?php
 
-if(preg_match_all('/^IMG_^images\/[0-9+]\.(jpg|png|gif)/','<img src="./images/56577.jpg",src="./images/33.jpg",src="./images/photo_5465178857633078337_y (1).png" alt="" width="300px">',$photo)){
-print_r($photo) ;
-}
-else{
-    echo"Розширення файла не коректне";
+if (preg_match_all('/^images\/IMG_[0-9]+\.(jpg|png|gif)/', '<img src="./images/56577.jpg",src="./images/33.jpg",src="./images/photo_5465178857633078337_y (1).png" alt="" width="300px">', $photo)) {
+    print_r($photo);
+} else {
+    echo "Розширення файла не коректне";
 }
