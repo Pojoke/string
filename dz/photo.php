721b523f3ef06491fd423537ba5200a8fda96180
Pojoke <pavlykdmutro@gmail.com>
2024-10-17 14:58:46 +0300

    email

--- dz/photo.php
+++ dz/photo.php
@@ -1,6 +1,6 @@
 <?php
 
-if (preg_match_all('/^images\/IMG_[0-9]+\.(jpg|png|gif)/', '<img src="./images/56577.jpg",src="./images/33.jpg",src="./images/photo_5465178857633078337_y (1).png" alt="" width="300px">', $photo)) {
+if (preg_match_all('/^images\/IMG_[0-9]+\.(jpg|png|gif)/', '<img src="./images/img_33.jpg",src="./images/photo_5465178857633078337_y (1).png" alt="" width="300px">', $photo)) {
     print_r($photo);
 } else {
     echo "Розширення файла не коректне";
--- str.php
+++ str.php
@@ -1,7 +1,13 @@
 <?php
 
-$str="[   34  555   8 9 9  ]";
-$prob=" ";
-$neprob="";
-$repl=str_replace($prob,$neprob,$str);
-echo $repl;
+// $str="[   34  555   8 9 9  ]";
+// $prob=" ";
+// $neprob="";
+// $repl=str_replace($prob,$neprob,$str);
+// echo $repl;
+$email='pavl@gmail.com';
+if (preg_match_all('/^[a-zA-Z][a-zA-Z0-9_]*@[a-zA-Z]+\.[a-zA-Z]/', $email)) {
+    print_r($email);
+} else {
+    echo "Email некоректний";
+}
