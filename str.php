c2beae8d4f0c6440b0d0ca2716301cef2e284ff7
Pojoke <pavlykdmutro@gmail.com>
2024-10-19 07:37:04 +0300

    yes

--- str.php
+++ str.php
@@ -6,7 +6,7 @@
 // $repl=str_replace($prob,$neprob,$str);
 // echo $repl;
 $email='pavl@gmail.com';
-if (preg_match_all('/^[a-zA-Z][a-zA-Z0-9_]*@[a-zA-Z]+\.[a-zA-Z]/', $email)) {
+if (preg_match_all('/^[a-zA-Z][a-zA-Z0-9_]*@[a-zA-Z]+\.[a-zA-Z]{3,6}/', $email)) {
     print_r($email);
 } else {
     echo "Email некоректний";
